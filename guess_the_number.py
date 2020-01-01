938bf4f3054317dbb3481ca7f12e061d9e6aeeef
Dhruv-Ji <59330771+Dhruv-Ji@users.noreply.github.com>
2020-01-01 18:03:29 +0530

    Second Method
    When you run your code then at last it prints:
    Your number is too low! You have  0 left!
    So, A good Solution is this

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,19 +1,15 @@
+import os
 import random
-
-
-golden_number = random.randint(0, 20)
-count = int(0)
-guess = int(3)
-
-while count != guess:
-    choice = int(input("Input a random number from 1 to 20: "))
-    if choice == golden_number:
-        print("Congratulations, you've found the golden number!")
-    elif choice < golden_number:
-        count += 1
-        print("Your number is too low! You have ", guess-count, "left!")
-    elif choice > golden_number:
-        count += 1
-        print("Your number is too large! You have ", guess-count, "left")
-        
-        
+# using for taking a random no
+a = random.randint(0, 100)
+print("Welcome to word gussing game. Guss a no between 0 to 100.")
+b = int(input('Enter No: '))
+while b != a:
+    if b < a:
+        print(b, ' is less then the no by', a - b)
+        b = int(input('Enter No: '))
+    elif b > a:
+        print(b, ' is greater then the no by', b - a)
+        b =int(input('Enter No: '))
+print('You WON!!     The No is ', a)
+os.system("pause")
